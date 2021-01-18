5edd65b30fb6a84e49dd876d6dbeb2da90f47133
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-01-18 15:33:49 -0500

    setting up dotenv and atlas

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require ("express");
+const mongoose = require ("mongoose"); 
+require ("dotenv").config()
+const app= express()
+const PORT= process.env.PORT || 8080; 
+
+//middleware 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+//connection 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true,
+    useFindAndModify: false}  )
+
+// routes 
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`app is running on http://localhost:${PORT}`);
+})
+
+
